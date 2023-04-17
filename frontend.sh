f9a82bd3f08e863237f1dcd3100d5ed25a565711
Juhi K <jkesarwani.us@gmail.com>
2023-04-17 19:06:08 -0400

    Create frontend conf file

--- frontend.sh
+++ frontend.sh
@@ -4,5 +4,6 @@ curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.z
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 # Add config file here
+cp roboshop.conf /etc/nginx/default.d/roboshop.conf
 systemctl enable nginx
 systemctl start nginx
