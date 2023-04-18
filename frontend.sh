c0ec77cfec881d1ea9b167d443f32fe58edad99e
Juhi K <jkesarwani.us@gmail.com>
2023-04-17 21:15:03 -0400

    Change Listen port mongo file

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,17 @@
+echo -e "\e[36m>>>>>>>>> Install NJINX server <<<<<<<<\e[0m"
 yum install nginx -y
+
+echo -e "\e[36m>>>>>>>>> Copy Config file <<<<<<<<\e[0m"
 cp roboshop.conf /etc/nginx/default.d/roboshop.conf
+
+echo -e "\e[36m>>>>>>>>> Remove frontend page <<<<<<<<\e[0m"
 rm -rf /usr/share/nginx/html/*
+
+echo -e "\e[36m>>>>>>>>> Download and unzip frontend files <<<<<<<<\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+
+echo -e "\e[36m>>>>>>>>> start NGINX server <<<<<<<<\e[0m"
 systemctl enable nginx
-systemctl start nginx
+systemctl restart nginx
