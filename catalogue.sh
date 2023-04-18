5e10bd2a56bc56b1e4692d5ffa08f2437d280b0e
Juhi K <jkesarwani.us@gmail.com>
2023-04-17 20:43:32 -0400

    Create mongo file

--- catalogue.sh
+++ catalogue.sh
@@ -7,8 +7,12 @@ curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue
 cd /app
 unzip /tmp/catalogue.zip
 npm install
+
 cp catalogue.service /etc/systemd/system/catalogue.service
+
 systemctl daemon-reload
+systemctl enable catalogue
+systemctl start catalogue
 
 # Mongo Schema
 cp mongodb.sh /etc/yum.repos.d/mongo.repo
