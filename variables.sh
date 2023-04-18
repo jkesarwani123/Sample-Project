f3869c40d827dc336d2c1a05fc9ee453db13594e
Juhi K <jkesarwani.us@gmail.com>
2023-04-17 21:35:08 -0400

    variable

---
+++ variables.sh
@@ -0,0 +1,14 @@
+a=10
+todaydate = 2023-04-17
+
+echo a= $a
+echo date = $date
+
+# VAR = $(Command)
+DATE = (date +%F)
+echo today date is $DATE
+
+# VAR = $((expression))
+ARTH = ((2+3/5))
+echo ARTH = ${ARTH}
+
