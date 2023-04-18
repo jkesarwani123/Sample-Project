cbf810812c1691a26ead7733b9d3288a5c89d99a
Juhi K <jkesarwani.us@gmail.com>
2023-04-17 21:06:02 -0400

    Change Listen port mongo file

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,11 @@
+echo -e "\e[36m>>>>>>>>> Install MongoDB <<<<<<<<\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y
+
+echo -e "\e[36m>>>>>>>>> Change Listen Port <<<<<<<<\e[0m"
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
+
+echo -e "\e[36m>>>>>>>>> start MongoDB service <<<<<<<<\e[0m"
 systemctl enable mongod
-systemctl start mongod
-# edit and replace port 127.0.0.1 to 0.0.0.0
+systemctl restart mongod
+
