4f6cab0edfbee3ab8b78be53989323f746b12c6a
Juhi K <jkesarwani.us@gmail.com>
2023-04-17 20:50:45 -0400

    Create mongo file

--- catalogue.sh
+++ catalogue.sh
@@ -2,10 +2,12 @@ curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
 useradd roboshop
 
+rm -rf /app
 mkdir /app
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
 unzip /tmp/catalogue.zip
+
 npm install
 
 cp catalogue.service /etc/systemd/system/catalogue.service
