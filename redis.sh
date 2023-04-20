f6315c6027a7079bfcd13391daa376892579bb36
Juhi K <jkesarwani.us@gmail.com>
2023-04-19 23:02:46 -0400

    Complete

--- redis.sh
+++ redis.sh
@@ -5,8 +5,7 @@ yum install redis -y
 
 # Change Listen port from 127.0.0.1 to 0.0.0.0
 echo -e "\e[36m>>>>>>>>> Change Listen Port <<<<<<<<\e[0m"
-sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf
-sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis/redis.conf
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf /etc/redis/redis.conf
 
 echo -e "\e[36m>>>>>>>>> start Redis service <<<<<<<<\e[0m"
 systemctl enable redis
