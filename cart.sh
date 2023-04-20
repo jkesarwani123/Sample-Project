e7662f846e756f3546d17f4149f1bcd08fdc0ad9
Juhi K <jkesarwani.us@gmail.com>
2023-04-20 12:35:34 -0400

    cart

--- cart.sh
+++ cart.sh
@@ -21,11 +21,11 @@ unzip /tmp/cart.zip
 echo -e "\e[36m>>>>>>>>> Install NodeJS Dependencies <<<<<<<<\e[0m"
 sudo npm install
 
-echo -e "\e[36m>>>>>>>>> Copy User SystemD file <<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>> Copy Cart SystemD file <<<<<<<<\e[0m"
 # cp catalogue.service /etc/systemd/system/catalogue.service
 cp /home/centos/Sample-Project/cart.service /etc/systemd/system/cart.service
 
-echo -e "\e[36m>>>>>>>>> Start User Service <<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>> Start Cart Service <<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable cart
 systemctl restart cart
