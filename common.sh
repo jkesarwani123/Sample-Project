19da9921d0c089b25385693c46d87891df2e94fb
Juhi K <jkesarwani.us@gmail.com>
2023-04-23 17:34:07 -0400

    cart

--- common.sh
+++ common.sh
@@ -1,7 +1,7 @@
 app_user=roboshop
-
+script=$(realpath "$0")
 script_path=$(dirname "$script")
-source ${script_path}/common.sh
+
 
 func_nodejs(){
   echo -e "\e[36m>>>>>>>>> Configuring NodeJS repos <<<<<<<<\e[0m"
