1b2502a94ae9f698c8e60778d9d1b918dc4cd693
Juhi K <jkesarwani.us@gmail.com>
2023-04-23 18:14:05 -0400

    cart

--- frontend.sh
+++ frontend.sh
@@ -2,20 +2,20 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
-echo -e "\e[36m>>>>>>>>> Install NJINX server <<<<<<<<\e[0m"
+print_head Install NJINX server
 yum install nginx -y
 
-echo -e "\e[36m>>>>>>>>> Copy Config file <<<<<<<<\e[0m"
+print_head Copy Config file
 cp roboshop.conf /etc/nginx/default.d/roboshop.conf
 
-echo -e "\e[36m>>>>>>>>> Remove frontend page <<<<<<<<\e[0m"
+print_head Remove frontend page
 rm -rf /usr/share/nginx/html/*
 
-echo -e "\e[36m>>>>>>>>> Download and unzip frontend files <<<<<<<<\e[0m"
+print_head Download and unzip frontend files
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-echo -e "\e[36m>>>>>>>>> start NGINX server <<<<<<<<\e[0m"
+print_head start NGINX server
 systemctl enable nginx
 systemctl restart nginx
--- mongodb.sh
+++ mongodb.sh
@@ -2,14 +2,14 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
-echo -e "\e[36m>>>>>>>>> Install MongoDB <<<<<<<<\e[0m"
+print_head Install MongoDB
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y
 
-echo -e "\e[36m>>>>>>>>> Change Listen Port <<<<<<<<\e[0m"
+print_head Change Listen Port
 sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
 
-echo -e "\e[36m>>>>>>>>> start MongoDB service <<<<<<<<\e[0m"
+print_head start MongoDB service
 systemctl enable mongod
 systemctl restart mongod
 
