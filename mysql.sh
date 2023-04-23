a5c365830eaeca279966a96526379a9c900b960e
Juhi K <jkesarwani.us@gmail.com>
2023-04-23 14:38:17 -0400

    cart

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,7 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
+sql_password=$1
 
 echo -e "\e[36m>>>>>>>>> Un-Install SQL <<<<<<<<\e[0m"
 dnf module disable mysql -y
@@ -16,5 +17,5 @@ systemctl enable mysqld
 systemctl start mysqld
 
 echo -e "\e[36m>>>>>>>>> set password for SQL <<<<<<<<\e[0m"
-mysql_secure_installation --set-root-pass RoboShop@1
-mysql -uroot -pRoboShop@1
+mysql_secure_installation --set-root-pass ${sql_password}
+mysql -uroot -p${sql_password}
