dfd0cd7c729d03b9ec7d1a07afabea95d4dddca7
Juhi K <jkesarwani.us@gmail.com>
2023-04-23 16:46:32 -0400

    cart

--- payment.sh
+++ payment.sh
@@ -1,6 +1,7 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
+rabbitmq_app_password=$1
 
 echo -e "\e[36m>>>>>>>>> Install Python <<<<<<<<\e[0m"
 yum install python36 gcc python3-devel -y
@@ -21,7 +22,7 @@ echo -e "\e[36m>>>>>>>>> Install Dependencies <<<<<<<<\e[0m"
 pip3.6 install -r requirements.txt
 
 echo -e "\e[36m>>>>>>>>> Copy payment SystemD file <<<<<<<<\e[0m"
-# cp catalogue.service /etc/systemd/system/catalogue.service
+sed -i -e "s|rabbitmq_app_password|${rabbitmq_app_password}" ${script_path}/payment.service
 cp ${script_path}/payment.service /etc/systemd/system/payment.service
 
 echo -e "\e[36m>>>>>>>>> Start payment Service <<<<<<<<\e[0m"
