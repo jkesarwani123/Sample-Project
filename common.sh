ddbe1f674459899f9a5a8c375666aaf138a4d808
Juhi K <jkesarwani.us@gmail.com>
2023-04-24 09:53:55 -0400

    cart

--- common.sh
+++ common.sh
@@ -19,13 +19,7 @@ schema_setup(){
   fi
 }
 
-func_nodejs(){
-  print_head Configuring NodeJS repos
-  curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-
-  print_head Install NodeJS
-  yum install nodejs -y
-
+func_prereq(){
   print_head Add Application User
   useradd ${app_user}
 
@@ -34,11 +28,22 @@ func_nodejs(){
   mkdir /app
 
   print_head Download App Content
-  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
   cd /app
 
   print_head Unzip App Content
   unzip /tmp/${component}.zip
+}
+
+func_nodejs(){
+  print_head Configuring NodeJS repos
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+  print_head Install NodeJS
+  yum install nodejs -y
+
+  print_head Install application content
+  func_prereq
 
   print_head Install NodeJS Dependencies
   sudo npm install
@@ -53,4 +58,30 @@ func_nodejs(){
   systemctl restart ${component}
 
   schema_setup
+}
+
+func_java(){
+print_head Install Maven for java
+yum install maven -y
+
+print_head Install application content
+func_prereq
+
+print_head Install Java Dependencies
+mvn clean package
+mv target/shipping-1.0.jar shipping.jar
+
+print_head Copy Shipping SystemD file
+# cp shipping.service /etc/systemd/system/shipping.service
+cp ${script_path}/shipping.service /etc/systemd/system/shipping.service
+
+print_head Load SQL Schema
+yum install mysql -y
+mysql -h mysql.jkdevops.online -uroot -p${mysql_password} < /app/schema/shipping.sql
+
+print_head Start Shipping Service
+systemctl daemon-reload
+systemctl enable shipping
+systemctl restart shipping
+
 }
--- shipping.sh
+++ shipping.sh
@@ -9,40 +9,8 @@ if [ -z "$mysql_password" ]; then
   else
     echo ${mysql_password}
 fi
-
-print_head Install Maven for java
-yum install maven -y
-
-print_head Add Application User
-useradd ${app_user}
-
-print_head Create Application Directory
-rm -rf /app
-mkdir /app
-
-print_head Download App Content
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
-cd /app
-
-print_head Unzip App Content
-unzip /tmp/shipping.zip
-
-print_head Install Java Dependencies
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
-
-print_head Copy Shipping SystemD file
-# cp shipping.service /etc/systemd/system/shipping.service
-cp ${script_path}/shipping.service /etc/systemd/system/shipping.service
-
-print_head Load SQL Schema
-yum install mysql -y
-mysql -h mysql.jkdevops.online -uroot -p${mysql_password} < /app/schema/shipping.sql
-
-print_head Start Shipping Service
-systemctl daemon-reload
-systemctl enable shipping
-systemctl restart shipping
-
+component=shipping
+schema_setup=mysql
+func_java
 
 
