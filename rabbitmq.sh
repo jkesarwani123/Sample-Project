fef65982846c13e358801f9917e06bc8931292fe
Juhi K <jkesarwani.us@gmail.com>
2023-04-24 23:21:42 -0400

    cart

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -11,21 +11,27 @@ if [ -z "$rabbitmq_app_password" ]; then
 fi
 
 print_head Setup ErLang Repos
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>$log_file
+func_status $?
 
 print_head Setup RabbitMQ Repos
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>$log_file
+func_status $?
 
 print_head Install ErLang
-yum install erlang -y
+yum install erlang -y &>>$log_file
+func_status $?
 
 print_head Install RabbitMQ
-yum install rabbitmq-server -y
+yum install rabbitmq-server -y &>>$log_file
+func_status $?
 
 print_head Start RabbitMQ Service
-systemctl enable rabbitmq-server
-systemctl start rabbitmq-server
+systemctl enable rabbitmq-server &>>$log_file
+systemctl start rabbitmq-server &>>$log_file
+func_status $?
 
 print_head create one user for the application
-rabbitmqctl add_user ${app_user} ${rabbitmq_app_password}
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+rabbitmqctl add_user ${app_user} ${rabbitmq_app_password} &>>$log_file
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$log_file
+func_status $?
