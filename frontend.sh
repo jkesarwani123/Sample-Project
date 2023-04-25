c7ab99d28d90dddf69ff80c57c712074f1d2444d
Juhi K <jkesarwani.us@gmail.com>
2023-04-24 22:49:50 -0400

    cart

--- frontend.sh
+++ frontend.sh
@@ -4,17 +4,27 @@ source ${script_path}/common.sh
 
 print_head Install NJINX server
 yum install nginx -y &>>$log_file
+ func_status $?
+
 print_head Copy Config file
 cp roboshop.conf /etc/nginx/default.d/roboshop.conf &>>$log_file
+ func_status $?
 
 print_head Remove frontend page
 rm -rf /usr/share/nginx/html/* &>>$log_file
+ func_status $?
 
-print_head Download and unzip frontend files
+print_head Download frontend files
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
+ func_status $?
+
+print_head unzip frontend files
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>$log_file
+ func_status $?
 
+print_head Start NGINX server
 print_head start NGINX server
 systemctl enable nginx &>>$log_file
 systemctl restart nginx &>>$log_file
+func_status $?
