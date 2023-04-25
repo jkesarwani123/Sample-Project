ff9c1023efb3bf295c24c3bb00ec91baa0cb169d
Juhi K <jkesarwani.us@gmail.com>
2023-04-24 23:00:34 -0400

    cart

--- mongodb.sh
+++ mongodb.sh
@@ -2,14 +2,20 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
-print_head Install MongoDB
+print_head Install MongoDB Repo
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$log_file
+func_status $?
+
+print_head Install MongoDB
 yum install mongodb-org -y &>>$log_file
+func_status $?
 
 print_head Change Listen Port
 sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
+func_status $?
 
 print_head start MongoDB service
 systemctl enable mongod &>>$log_file
 systemctl restart mongod &>>$log_file
+func_status $?
 
